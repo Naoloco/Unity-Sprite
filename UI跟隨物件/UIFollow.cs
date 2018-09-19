f0e3e399cc42577f7575fca12ef6f79ce1ea10b9
Naoloco <43401389+Naoloco@users.noreply.github.com>
2018-09-19 15:36:54 +0800

    Add files via upload

---
+++ UI跟隨物件/UIFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIFollow : MonoBehaviour
+{
+    public Vector2 offset;
+    public RectTransform rectTransform; // 跟随UI
+
+    void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        rectTransform.position = screenPos + new Vector2(offset.x, offset.y);
+
+        /*if (screenPos.x > Screen.width || screenPos.x < 0 || screenPos.y > Screen.height || screenPos.y < 0)
+            rectTransform.gameObject.SetActive(false);
+        else
+            rectTransform.gameObject.SetActive(true);*/
+    }
+
+}
